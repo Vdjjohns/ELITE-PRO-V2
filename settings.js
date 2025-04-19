09c74e8236e6c7e720801bf2ac9b9de8c7b07cfe
Vdjjohns <155607501+Vdjjohns@users.noreply.github.com>
2025-04-19 15:18:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2\n\n+2348109263390"
 global.creator = "2348109263390@s.whatsapp.net"
-global.xprefix = '.' //set any prefix 
+global.xprefix = '#' //set any prefix 
 global.premium = ["254108988663"] // Premium User
 //Stop here. Thanks for choosing ELITEPRO.
 
